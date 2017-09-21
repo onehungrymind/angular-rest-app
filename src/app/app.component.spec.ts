973da014431346518775bc08ca04c3e8a94ded89
Jonathan Garvey <jon.d.garvey@gmail.com>
2017-09-20 17:51:48 -0700

    added some tests to app component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,29 +1,81 @@
 import 'hammerjs';
 
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { DebugElement } from '@angular/core';
+import { DebugElement, QueryList } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AppMaterialModule } from './app-material.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterTestingModule } from '@angular/router/testing';
+import { RouterOutletStub } from '../testing/router.stubs';
+import { RouterOutlet } from '@angular/router';
+import { By } from '@angular/platform-browser';
 
 describe('AppComponent', () => {
   let component: AppComponent;
   let fixture: ComponentFixture<AppComponent>;
   let de: DebugElement;
+  let router: RouterOutletStub;
+  let links: DebugElement[];
 
   beforeEach(() => {
     fixture = TestBed.configureTestingModule({
       imports: [ AppMaterialModule, BrowserAnimationsModule, RouterTestingModule ],
       declarations: [ AppComponent ],
+      providers: [{provide: RouterOutlet, useClass: RouterOutletStub}]
     }).createComponent(AppComponent);
 
     component = fixture.componentInstance;
     de = fixture.debugElement;
+    router = fixture.debugElement.injector.get(RouterOutlet);
     fixture.detectChanges();
+
+    links = de.queryAll(By.css('nav a'));
   });
 
-  it('should create the app', () => {
-    expect(component).toBeTruthy();
+  it('should have a title called `Angular REST App`', () => {
+    expect(component.title).toBe('Angular REST App');
+  });
+
+  it('should have four links', () => {
+    expect(links.length).toBe(4);
+  });
+
+  it('should link to `home`', () => {
+    const link = links.find(l => l.properties.href === '/home');
+    expect(link).toBeDefined();
+  });
+
+  it('should link to `items`', () => {
+    const link = links.find(l => l.properties.href === '/items');
+    expect(link).toBeDefined();
+  });
+
+  it('should link to `widgets`', () => {
+    const link = links.find(l => l.properties.href === '/widgets');
+    expect(link).toBeDefined();
+  });
+
+  it('should link to `profile`', () => {
+    const link = links.find(l => l.properties.href === '/profile');
+    expect(link).toBeDefined();
+  });
+
+  describe('#prepareRouterState', () => {
+    it('should return `Router.activatedRouteData.animation` if it has a value', () => {
+      router.activatedRouteData = {animation: 'foo'};
+      const result = component.prepareRouterState(router as RouterOutlet);
+      expect(result).toBe('foo');
+    });
+
+    it('should return `initial` by default', () => {
+      router.activatedRouteData = {};
+      const result = component.prepareRouterState(router as RouterOutlet);
+      expect(result).toBe('initial');
+    });
+  });
+
+  it('should have an @routerAnimations trigger set to `initial`', () => {
+    const routerContainer = de.query(By.css('.app-content'));
+    expect(routerContainer.properties['@routerAnimations']).toBe('initial');
   });
 });
---
+++ src/testing/router.stubs.ts
@@ -0,0 +1,5 @@
+export class RouterOutletStub {
+  activatedRouteData: {
+    [name: string]: any;
+  };
+}
